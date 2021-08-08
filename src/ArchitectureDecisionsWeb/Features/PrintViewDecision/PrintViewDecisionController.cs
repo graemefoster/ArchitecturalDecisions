075012226c80b237f284b518e65a81b63187b9eb
Graeme Foster <graemefoster@gmail.com>
2021-08-08 10:30:23 +0800

    Adding viewer

---
+++ src/ArchitectureDecisionsWeb/Features/PrintViewDecision/PrintViewDecisionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using ArchitectureDecisionsCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectureDecisionsWeb.Features.PrintViewDecision
+{
+    public class PrintViewDecisionController : Controller
+    {
+        private readonly IDecisionRepository _repository;
+
+        public PrintViewDecisionController(IDecisionRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        // GET
+        public async Task<IActionResult> Execute(Guid id)
+        {
+            return View(new ViewModel()
+            {
+                Decision = (await _repository.Get(id))
+            });
+        }
+    }
+}
---
+++ src/ArchitectureDecisionsWeb/Features/PrintViewDecision/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ArchitectureDecisionsCore;
+
+namespace ArchitectureDecisionsWeb.Features.PrintViewDecision
+{
+    public class ViewModel
+    {
+        public Decision Decision { get; set; } = null!;
+    }
+}
